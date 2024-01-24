a8d40a334c6df8df23d19511b80142257e7a1940
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2024-01-24 13:57:18 +0100

    Clues on how view-models are persisted

--- app/src/main/java/ru/otus/dz_2024_01/MainActivity.kt
+++ app/src/main/java/ru/otus/dz_2024_01/MainActivity.kt
@@ -1,7 +1,7 @@
 package ru.otus.dz_2024_01
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -10,4 +10,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    /**
+     * Получить данные экземпляра, не относящиеся к конфигурации,
+     * которые ранее были возвращены функцией onRetainNonConfigurationInstance().
+     */
+//    override fun getLastNonConfigurationInstance(): Any? {
+//        return super.getLastNonConfigurationInstance()
+//    }
+
+    /**
+     * Вызывается системой в рамках уничтожения activity из-за
+     * изменения конфигурации, когда известно, что для новой
+     * конфигурации будет немедленно создан новый экземпляр.
+     * Здесь вы можете вернуть любой объект, который вам нравится,
+     * включая сам экземпляр activity, который впоследствии можно получить,
+     * вызвав getLastNonConfigurationInstance() в новом экземпляре activity.
+     */
+//    override fun onRetainNonConfigurationInstance(): Any? {
+//        return super.onRetainNonConfigurationInstance()
+//    }
 }
